11c22c38c09c3310abf5d4f86f3948d15aed83f5
linhdau1 <66782148+linhdau1@users.noreply.github.com>
2020-12-17 22:22:59 +0700

    Update B.js

--- bài 3/B.js
+++ bài 3/B.js
@@ -3,6 +3,11 @@ let count  = 3;
 console.log(number);
 
  $('#check').click(function(){
+	
+	if($('#input_text').val() > 10 && $('#input_text').val() < 1)
+	{
+		alert("Dự đoán trong từ 1 đến 10, vui lòng nhập lại")
+	}
 	if(number == $('#input_text').val()){
 		alert($('#input_text').val() + ' là kết quả đúng');
 	}
